5d824d075c74a2cd6ccbabcd1786d27cb5eb9ba3
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-15 07:52:40 -0700

    Add CSS and content for home

--- src/home.js
+++ src/home.js
@@ -1,1 +1,36 @@
-export const home = "Hello, Odinite!";
+
+const content = document.getElementById('content')
+
+const home = function() {
+	header()
+	tab()
+	welcome()
+}
+
+
+const header = function() {
+	const title = document.createElement("h2")
+	const node = document.createTextNode("Home");
+	title.appendChild(node);
+
+	content.appendChild(title);
+}
+
+const tab = function() {
+	const tabName = document.createElement("h3")
+	const node = document.createTextNode("Tasty meals and even tastier meals!");
+	tabName.appendChild(node);
+
+	content.appendChild(tabName);
+}
+
+const welcome = function() {
+	const welcomeParagraph = document.createElement("p")
+	const node = document.createTextNode("At Tasty Meals we believe that soul is at the heart of every meal.");
+	welcomeParagraph.appendChild(node);
+
+	content.appendChild(welcomeParagraph);
+}
+
+
+export {home};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./styles.css";
 import { home } from "./home.js";
 
-console.log(home)
+console.log(home())
